c11861b2fb1ff8fc823075fd45c9bdc034c5b903
Sushant Yadav <76422301+sushant7667@users.noreply.github.com>
2023-07-27 21:52:07 +0530

    urls.py

--- Usermaster/user_frontend/urls.py
+++ Usermaster/user_frontend/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from user_frontend import views
 
 urlpatterns = [
+    path('hello_world', views.hello_world,name="hello_world"),
     path('Adduser_front', views.Adduser_front,name="Adduser_front"),
     path('Getuser_Front', views.Getuser_Front,name="Getuser_Front"),
     path('Updateuser_front/<int:id>', views.Updateuser_front,name="Updateuser_front"),
